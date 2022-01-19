dc0b7af43afffa793e74c240dd50ef1d3566d4d8
tanishq <tanishqg2406@gmail.com>
2022-01-19 15:08:55 +0530

    REL update

--- relation_classification/relation_classification.py
+++ relation_classification/relation_classification.py
@@ -39,10 +39,13 @@ def ensure_dir(dir_path):
 
 
 parser = ArgumentParser()
-parser.add_argument('--model_name', required=True, choices=['scibert', 'matscibert'], type=str)
+parser.add_argument('--model_name', required=True, choices=['scibert', 'matscibert', 'bert'], type=str)
 parser.add_argument('--model_save_dir', required=True, type=str)
 parser.add_argument('--preds_save_dir', default=None, type=str)
 parser.add_argument('--cache_dir', default=None, type=str)
+parser.add_argument('--seeds', nargs='+', default=None, type=int)
+parser.add_argument('--lm_lrs', nargs='+', default=None, type=float)
+parser.add_argument('--non_lm_lr', default=3e-4, type=float)
 args = parser.parse_args()
 
 if args.model_name == 'scibert':
@@ -51,6 +54,9 @@ if args.model_name == 'scibert':
 elif args.model_name == 'matscibert':
     model_name = 'm3rg-iitd/matscibert'
     to_normalize = True
+elif args.model_name == 'bert':
+    model_name = 'bert-base-uncased'
+    to_normalize = False
 else:
     raise NotImplementedError
 
@@ -58,6 +64,11 @@ model_revision = 'main'
 cache_dir = ensure_dir(args.cache_dir) if args.cache_dir else None
 output_dir = ensure_dir(args.model_save_dir)
 preds_save_dir = ensure_dir(args.preds_save_dir) if args.preds_save_dir else None
+
+if args.seeds is None:
+    args.seeds = [0, 1, 2]
+if args.lm_lrs is None:
+    args.lm_lrs = [2e-5, 3e-5, 5e-5]
 
 data_dir = 'datasets/annotated-materials-syntheses'
 
@@ -275,12 +286,14 @@ best_test_oth_list = None
 
 num_epochs = 10
 
-for lr in [2e-5, 3e-5, 5e-5]:
+for lr in args.lm_lrs:
+
     print(f'lr: {lr}')
     val_acc, val_oth = [], []
     test_acc, test_oth = [], []
     
-    for SEED in [0, 1, 2]:
+    for SEED in args.seeds:
+        
         print(f'SEED: {SEED}')
 
         torch.set_deterministic(True)
@@ -297,6 +310,7 @@ for lr in [2e-5, 3e-5, 5e-5]:
             load_best_model_at_end=True,
             metric_for_best_model=metric_for_best_model,
             greater_is_better=True,
+            save_total_limit=2,
             warmup_ratio=0.1,
             learning_rate=lr,
             seed=SEED
@@ -305,7 +319,7 @@ for lr in [2e-5, 3e-5, 5e-5]:
         model = BERT_RC(model_name).to(device)
         
         optimizer_grouped_parameters = [
-            {'params': [p for n, p in model.named_parameters() if not 'bert' in n], 'lr': 3e-4},
+            {'params': [p for n, p in model.named_parameters() if not 'bert' in n], 'lr': args.non_lm_lr},
             {'params': [p for n, p in model.named_parameters() if 'bert' in n], 'lr': lr}
         ]
         optimizer_kwargs = {
--- relation_classification/run.sh
+++ relation_classification/run.sh
@@ -4,7 +4,7 @@ model_save_dir=/scratch/maths/dual/mt6170499/relation_classification/model
 preds_save_dir=/scratch/maths/dual/mt6170499/relation_classification/preds
 cache_dir=/scratch/maths/dual/mt6170499/.cache
 
-for model_name in matscibert scibert; do
+for model_name in matscibert scibert bert; do
     echo $model_name
     python -u relation_classification.py --model_name $model_name --model_save_dir $model_save_dir --preds_save_dir $preds_save_dir --cache_dir $cache_dir
 done
